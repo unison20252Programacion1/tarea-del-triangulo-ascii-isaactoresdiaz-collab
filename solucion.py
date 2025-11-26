29c41fe1e1ae89b6534883b96c367905699eb19f
isaactoresdiaz-collab <isaactoresdiaz@gmail.com>
2025-11-25 22:15:00 -0700

    Update solucion.py

--- solucion.py
+++ solucion.py
@@ -1,5 +1,4 @@
 # En este archivo debes implementar la función
-
 def triangulo_simetrico(m: int, s: str) -> str:
     # TODO: validar altura mayor que 0 e imprimir "Error: La altura debe ser un entero positivo" y salir
     if m<= 0:
